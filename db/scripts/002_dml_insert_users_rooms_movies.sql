356b7ccbff5275bcf803e5e82095e87f88065bd3
nikez <nikegreen@yandex.ru>
2023-01-11 14:16:58 +0500

    2. Сервис - Кинотеатр [#504869 #378991] изменил скрипты sql.

---
+++ db/scripts/002_dml_insert_users_rooms_movies.sql
@@ -0,0 +1,35 @@
+
+insert into users(username, password, email, phone)
+values ('admin', 'admin', 'admin@email.ru', '+79999999999');
+insert into users(username, password, email, phone)
+values ('Гость', '', '', '');
+
+insert into movies(name, filename)
+values ('непослушник2', 'images1.jpg');
+insert into movies(name, filename)
+values ('3000 лет желаний', 'images2.jpg');
+insert into movies(name, filename)
+values ('отряд призрак', 'images3.jpg');
+insert into movies(name, filename)
+values ('русалка и дочь короля', 'images4.jpg');
+insert into movies(name, filename)
+values ('бывшие', 'images5.jpg');
+insert into movies(name, filename)
+values ('чук и гек большие приключения', 'images6.jpg');
+insert into movies(name, filename)
+values ('паранормальные явления. дом призраков', 'images7.jpg');
+insert into movies(name, filename)
+values ('вышка', 'images8.jpg');
+insert into movies(name, filename)
+values ('мира. 2 семьи. космос не расстояние', 'images9.jpg');
+insert into movies(name, filename)
+values ('тень. взять гордея', 'images10.jpg');
+insert into movies(name, filename)
+values ('астрал. реинкарнация', 'images11.jpg');
+insert into movies(name, filename)
+values ('на ощупь', 'images12.jpg');
+
+insert into rooms(name) values ('малый зал');
+insert into rooms(name) values ('средний зал');
+insert into rooms(name) values ('большой зал');
+
